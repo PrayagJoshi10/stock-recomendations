96c18676297af30927d481dcc0af1ea25871537e
Prayag Joshi <prayagjoshi263@gmail.com>
2024-01-05 11:08:28 +0530

    Changed auth logic and designed splash screen

--- src/screens/splash/Splash.tsx
+++ src/screens/splash/Splash.tsx
@@ -1,12 +1,39 @@
-import {StyleSheet, View} from 'react-native';
-import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import Colors from '../../utils/Colors';
-import Loader from '../../components/loaders/Loader';
+import {getData} from '../../utils/Helper';
+import {ROUTES} from '../../navigation/Routes';
+import Fonts from '../../utils/Fonts';
 
-const Splash = () => {
+interface splashProps {
+  navigation: any;
+}
+const Splash = ({navigation}: splashProps) => {
+  const [isProccessing, setisProccessing] = useState<boolean>(true);
+
+  useEffect(() => {
+    const checkIsLoggedIn = async () => {
+      const isLoggedInValue = await getData('isLoggedIn');
+
+      if (!isProccessing) {
+        if (isLoggedInValue) {
+          navigation.replace(ROUTES.MAINSTACK.STACK);
+        } else {
+          navigation.replace(ROUTES.AUTHSTACK.STACK);
+        }
+      }
+    };
+
+    setTimeout(() => {
+      setisProccessing(false);
+    }, 1000);
+
+    checkIsLoggedIn();
+  }, [isProccessing, navigation]);
+
   return (
     <View style={styles.container}>
-      <Loader />
+      <Text style={styles.splashTest}>S t o c k</Text>
     </View>
   );
 };
@@ -17,5 +44,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.white,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  splashTest: {
+    color: Colors.gray_700,
+    fontSize: 32,
+    fontFamily: Fonts.urbanist_800,
   },
 });
