f479339a0a91e2fda41bf68318f2731943e781ae
Prayag Joshi <prayagjoshi263@gmail.com>
2024-01-05 15:42:23 +0530

    added basic splash test animation

--- src/screens/splash/Splash.tsx
+++ src/screens/splash/Splash.tsx
@@ -1,15 +1,21 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import Colors from '../../utils/Colors';
 import {getData} from '../../utils/Helper';
 import {ROUTES} from '../../navigation/Routes';
 import Fonts from '../../utils/Fonts';
+import {MotiText} from 'moti';
+import {responsiveFontSize} from 'react-native-responsive-dimensions';
 
 interface splashProps {
   navigation: any;
 }
 const Splash = ({navigation}: splashProps) => {
   const [isProccessing, setisProccessing] = useState<boolean>(true);
+  const fromTopToBottom = {opacity: 0, translateY: -100};
+  const animateTopToBotton = {opacity: 1, translateY: 0};
+  const fromBottomToTop = {opacity: 0, translateY: 100};
+  const animateBottomToTop = {opacity: 1, translateY: 0};
 
   useEffect(() => {
     const checkIsLoggedIn = async () => {
@@ -30,10 +36,45 @@ const Splash = ({navigation}: splashProps) => {
 
     checkIsLoggedIn();
   }, [isProccessing, navigation]);
+  useEffect(() => {}, []);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.splashTest}>S t o c k</Text>
+      <MotiText
+        style={styles.splashTest}
+        from={fromTopToBottom}
+        animate={animateTopToBotton}
+        transition={{type: 'timing', duration: 700}}>
+        S
+      </MotiText>
+      <MotiText
+        style={styles.splashTest}
+        from={fromBottomToTop}
+        animate={animateBottomToTop}
+        transition={{type: 'timing', duration: 700}}>
+        t
+      </MotiText>
+      <MotiText
+        style={styles.splashTest}
+        from={fromTopToBottom}
+        animate={animateTopToBotton}
+        transition={{type: 'timing', duration: 700}}>
+        o
+      </MotiText>
+      <MotiText
+        style={styles.splashTest}
+        from={fromBottomToTop}
+        animate={animateBottomToTop}
+        transition={{type: 'timing', duration: 700}}>
+        c
+      </MotiText>
+      <MotiText
+        style={styles.splashTest}
+        from={fromTopToBottom}
+        animate={animateTopToBotton}
+        transition={{type: 'timing', duration: 700}}>
+        k
+      </MotiText>
     </View>
   );
 };
@@ -46,10 +87,12 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.white,
     justifyContent: 'center',
     alignItems: 'center',
+    flexDirection: 'row',
+    gap: 15,
   },
   splashTest: {
     color: Colors.gray_700,
-    fontSize: 32,
+    fontSize: responsiveFontSize(6),
     fontFamily: Fonts.urbanist_800,
   },
 });
